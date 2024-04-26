528aaa1969bd6e6792c89bf7b481a34484ce8538
jujuketley <julianaketley101@gmail.com>
2024-04-26 09:55:14 -0500

    atividade

---
+++ script.js
@@ -0,0 +1,38 @@
+const dom = 1;
+const seg = 2;
+const ter = 3;
+const qua = 4;
+const qui = 5;
+const sex = 6;
+const sab = 7;
+
+let dia = 2;
+
+if (dia ==1){
+
+    document.write("Domingo dia de jogar bola");
+}
+else if (dia ==2){
+
+    document.write("Segunda dia de estudar");
+}
+else if (dia ==3){
+
+    document.write("Terça dia de ir treinar");
+}
+else if (dia ==4){
+
+    document.write("Quarta dia de tomar teres");
+}
+else if (dia ==5){
+
+    document.write("Quinta dia de ouvir musica");
+}
+else if (dia ==6){
+
+    document.write("Sexta dia de correr");
+}
+else if (dia ==7){
+
+    document.write("Sabado dia de refeiçao livre");
+}
